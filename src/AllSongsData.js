b475a0555e65d83a9c2ab497ac024a805d4efa68
Subham Tripathy <subhamtripathy962@gmail.com>
2025-08-24 19:34:15 +0530

    updated the songs data

--- src/AllSongsData.js
+++ src/AllSongsData.js
@@ -2,28 +2,28 @@ export const playlist = [
     {
         id: 1,
         title: "Besharam Title Track",
-        duration: "3:45",
+        duration: "3:38",
         cover: "/assets/covers/besharam-title-track.jpg",
         audio: "/assets/songs/besharam-title-track.mp3"
     },
     {
-        id: 1,
+        id: 2,
         title: "Naam Apun-ka-jaani",
-        duration: "3:45",
+        duration: "4:46",
         cover: "/assets/covers/makkhi-naam-apun-ka-jaani.jpg",
         audio: "/assets/songs/makkhi-naam-apun-ka-jaani.mp3"
     },
     {
-        id: 1,
-        title: "Besharam Title Track",
-        duration: "3:45",
+        id: 3,
+        title: "Jumme Ki Raat",
+        duration: "4:33",
         cover: "/assets/covers/kick-jumme-ki-raat.jpg",
         audio: "/assets/songs/kick-jumme-ki-raat.mp3"
     },
     {
-        id: 1,
-        title: "Besharam Title Track",
-        duration: "3:45",
+        id: 4,
+        title: "Jawan - Title Track",
+        duration: "3:08",
         cover: "/assets/covers/jawan-title-track.jpg",
         audio: "/assets/songs/jawan-title-track.mp3"
     },
